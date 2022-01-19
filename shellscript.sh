9b89f37aaa280027a2cf25736b220f01634867bf
mssk512 <mskrishna512@gmail.com>
2022-01-19 13:55:38 +0530

    added shellscript file

---
+++ shellscript.sh
@@ -0,0 +1,20 @@
+#################### shell script ##################################
+5.	gitconfig  - -global user.email “emilid”
+6.	gitconfig  - -global user.name “name”
+
+#Clone the code from github repo
+7.	git clone repoURL-link
+8.	cd reponame
+9.	ls -ll
+#modify the files
+Create file = CMD: touch filename
+Edit and add the data in file  = CMD: vi filename  I (insert) write the data  press esc  :wq! (to save the data)
+Save the file
+
+
+10.	git status (red colour)
+11.	git add .
+12.	git status
+13.	git commit -m “message”
+14.	git push (if required give the username and password)
+
